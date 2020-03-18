f8e06439285bfb55352b728aab41dc03dbd7abfd
JD-93 <kachare.jaydeep@gmail.com>
2020-03-18 11:39:21 +0530

    Update 01.cpp

--- 01 accept and display array/src/01.cpp
+++ 01 accept and display array/src/01.cpp
@@ -15,8 +15,8 @@ int main() {
 	accept_array(arr) ;
 	display_array(arr) ;
 
-//	int n= sizeof(arr)/sizeof(int)
-		// can use here in local scope  ---- not to be used in function see accept_array(int arr[]) ;
+	//	int n= sizeof(arr)/sizeof(int)
+	// can use here in local scope  ---- not to be used in function see accept_array(int arr[]) ;
 
 	return 0;
 }
@@ -24,8 +24,8 @@ int main() {
 void accept_array( int arr[] )
 {
 
-//	int n = sizeof(arr)/int(size) ;
-			// can not use b/c arr size will be 4 bytes as it is passed as pointer
+	//	int n = sizeof(arr)/int(size) ;
+	// can not use b/c arr size will be 4 bytes as it is passed as pointer
 
 	for (int i=0 ; i<size ; i++)	{
 		cout<<"Enter array element arr["<<i<<"] :  " ;
